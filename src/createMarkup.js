8e894fa8fd9ce132340d0012a7182c6a940aa4e9
Yurii <dets.yurii@gmail.com>
2022-11-25 13:32:14 +0200

    task complete

---
+++ src/createMarkup.js
@@ -0,0 +1,27 @@
+export { prewiewMarkup, countryInfoMurkup };
+
+function prewiewMarkup({ flags, name }) {
+  return `<li class="country-list__item">
+        <img
+          class="country-list__flag"
+          src="${flags.svg}"
+          width="30px"
+          height="20px"
+        />
+        <p class="country-list__name">${name.official}</p>
+      </li>`;
+}
+
+function countryInfoMurkup({ flags, name, capital, population, languages }) {
+  const langStr = Object.values(languages).join(', ');
+
+  return ` 
+      <div class="country-list__item">
+      <img class="country-list__flag" width="60px" height="40px" src="${flags.svg}"></img>
+      <p class="country-list__name accent">${name.official}</p></div>
+      <div class="description">
+      <p class="description__name">Capital: <span class="description__second-name" >${capital}</span><p>
+      <p class="description__name">Population: <span class="description__second-name">  ${population}</span></p>
+      <p class="description__name">Languages: <span class="description__second-name" >${langStr}</span></p>
+      </div>`;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,70 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+import { prewiewMarkup, countryInfoMurkup } from './createMarkup';
 
+const refs = {
+  inputRef: document.querySelector('#search-box'),
+  countryListRef: document.querySelector('.country-list'),
+  countryInfoRef: document.querySelector('.country-info'),
+};
 const DEBOUNCE_DELAY = 300;
+const NOTIFY_TIMEOUT = 2000;
+
+refs.inputRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+let inputValue = '';
+
+function onSearch(event) {
+  clearCountryHTML();
+  inputValue = event.target.value.trim().toLowerCase();
+
+  if (!inputValue) {
+    return;
+  }
+
+  fetchCountries(inputValue)
+    .then(data => {
+      if (data.length === 1) {
+        renderCountryInfoMarkup(data);
+      } else if (data.length >= 2 && data.length <= 10) {
+        renderPrewiewMarkup(data);
+      } else {
+        onManyCoincidences();
+      }
+    })
+    .catch(error => {
+      onResponseError();
+    });
+}
+
+function renderPrewiewMarkup(data) {
+  const markup = data.map(prewiewMarkup).join('');
+  refs.countryListRef.innerHTML = markup;
+}
+
+function renderCountryInfoMarkup(data) {
+  const markup = data.map(countryInfoMurkup).join('');
+  refs.countryInfoRef.innerHTML = markup;
+}
+
+function clearCountryHTML() {
+  refs.countryListRef.innerHTML = '';
+  refs.countryInfoRef.innerHTML = '';
+}
+
+function onManyCoincidences() {
+  return Notify.info(
+    'Too many matches found. Please enter a more specific name.',
+    {
+      timeout: NOTIFY_TIMEOUT,
+    }
+  );
+}
+
+function onResponseError() {
+  return Notify.failure('Oops, there is no country with that name', {
+    timeout: NOTIFY_TIMEOUT,
+  });
+}
